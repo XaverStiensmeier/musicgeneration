9e382e1797fe38e7daa4e81c5e18f71f7ed86be7
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-06-12 23:16:43 +0200

    Added generator

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # See https://www.tensorflow.org/tutorials/audio/music_generation
+# Temperature https://www.tensorflow.org/text/tutorials/text_generation
 
 import collections
 import datetime
@@ -64,6 +65,7 @@ def plot_distributions(notes: pd.DataFrame, drop_percentile=2.5):
     plt.subplot(1, 3, 3)
     max_duration = np.percentile(notes['duration'], 100 - drop_percentile)
     sns.histplot(notes, x="duration", bins=np.linspace(0, max_duration, 21))
+    plt.show()
 
 
 def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
@@ -90,8 +92,8 @@ def display_audio(pm: pretty_midi.PrettyMIDI, seconds=30):
     return display.Audio(waveform_short, rate=_SAMPLING_RATE)
 
 
-def save_audio(pm: pretty_midi.PrettyMIDI):
-    pm.write('done.mid')
+def save_audio(pm: pretty_midi.PrettyMIDI, name='done.mid'):
+    pm.write(name)
 
 
 def midi_to_notes(midi_file: str) -> pd.DataFrame:
@@ -117,19 +119,6 @@ def midi_to_notes(midi_file: str) -> pd.DataFrame:
 
 
 def do_stuff():
-    seed = 42
-    tf.random.set_seed(seed)
-    np.random.seed(seed)
-
-    data_dir = pathlib.Path('data/maestro-v2.0.0')
-    if not data_dir.exists():
-        tf.keras.utils.get_file(
-            'maestro-v2.0.0-midi.zip',
-            origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
-            extract=True,
-            cache_dir='.', cache_subdir='data',
-        )
-    filenames = glob.glob(str(data_dir / '**/*.mid*'))
     print('Number of files:', len(filenames))
     sample_file = filenames[1]
     print(sample_file)
@@ -166,18 +155,237 @@ def do_stuff():
     # plot all notes
     plot_piano_roll(raw_notes)
     # plot node distribution
+    print("Distributions")
     plot_distributions(raw_notes)
 
     # write your own midi
-    example_file = 'example.midi'
-    example_pm = notes_to_midi(
-        raw_notes, out_file=example_file, instrument_name=instrument_name)
+    # example_file = 'example.midi'
+    # example_pm = notes_to_midi(
+    #    raw_notes, out_file=example_file, instrument_name=instrument_name)
     # display_audio(example_pm)
-    save_audio(example_pm)
+    # save_audio(example_pm)
+
+
+def create_sequences(
+        dataset: tf.data.Dataset,
+        seq_length: int,
+        vocab_size=128,
+) -> tf.data.Dataset:
+    seq_length = seq_length + 1
+
+    # Take 1 extra for the labels
+    windows = dataset.window(seq_length, shift=1, stride=1,
+                             drop_remainder=True)
+
+    # `flat_map` flattens the" dataset of datasets" into a dataset of tensors
+    flatten = lambda x: x.batch(seq_length, drop_remainder=True)
+    sequences = windows.flat_map(flatten)
+
+    # Normalize note pitch
+    def scale_pitch(x):
+        x = x / [vocab_size, 1.0, 1.0]
+        return x
+
+    # Split the labels
+    def split_labels(sequences):
+        inputs = sequences[:-1]
+        labels_dense = sequences[-1]
+        key_order = ['pitch', 'step', 'duration']
+        labels = {key: labels_dense[i] for i, key in enumerate(key_order)}
+        return scale_pitch(inputs), labels
+
+    return sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
+
+
+def mse_with_positive_pressure(y_true: tf.Tensor, y_pred: tf.Tensor):
+    mse = (y_true - y_pred) ** 2
+    positive_pressure = 10 * tf.maximum(-y_pred, 0.0)
+    return tf.reduce_mean(mse + positive_pressure)
+
+
+def predict_next_note(
+        notes: np.ndarray,
+        keras_model: tf.keras.Model,
+        temperature: float = 1.0) -> (int, float, float):
+    """Generates a note IDs using a trained sequence model."""
+
+    assert temperature > 0
+
+    # Add batch dimension
+    inputs = tf.expand_dims(notes, 0)
+
+    predictions = keras_model.predict(inputs)
+    pitch_logits = predictions['pitch']
+    step = predictions['step']
+    duration = predictions['duration']
+
+    pitch_logits /= temperature
+    pitch = tf.random.categorical(pitch_logits, num_samples=1)
+    pitch = tf.squeeze(pitch, axis=-1)
+    duration = tf.squeeze(duration, axis=-1)
+    step = tf.squeeze(step, axis=-1)
+
+    # `step` and `duration` values should be non-negative
+    step = tf.maximum(0, step)
+    duration = tf.maximum(0, duration)
+
+    return int(pitch), float(step), float(duration)
+
+
+def train():
+    num_files = 5
+    all_notes = []
+    for f in filenames[:num_files]:
+        notes = midi_to_notes(f)
+        all_notes.append(notes)
+    all_notes = pd.concat(all_notes)
+    n_notes = len(all_notes)
+    print('Number of notes parsed:', n_notes)
+
+    key_order = ['pitch', 'step', 'duration']
+    train_notes = np.stack([all_notes[key] for key in key_order], axis=1)
+
+    notes_ds = tf.data.Dataset.from_tensor_slices(train_notes)
+    seq_length = 25
+    vocab_size = 128
+    seq_ds = create_sequences(notes_ds, seq_length, vocab_size)
+    for seq, target in seq_ds.take(1):
+        print('sequence shape:', seq.shape)
+        print('sequence elements (first 10):', seq[0: 10])
+        print()
+        print('target:', target)
+
+    batch_size = 64
+    buffer_size = n_notes - seq_length  # the number of items in the dataset
+    train_ds = (seq_ds
+                .shuffle(buffer_size)
+                .batch(batch_size, drop_remainder=True)
+                .cache()
+                .prefetch(tf.data.experimental.AUTOTUNE))
+
+    input_shape = (seq_length, 3)
+    learning_rate = 0.005
+
+    inputs = tf.keras.Input(input_shape)
+    x = tf.keras.layers.LSTM(128)(inputs)
+
+    outputs = {
+        'pitch': tf.keras.layers.Dense(128, name='pitch')(x),
+        'step': tf.keras.layers.Dense(1, name='step')(x),
+        'duration': tf.keras.layers.Dense(1, name='duration')(x),
+    }
+
+    model = tf.keras.Model(inputs, outputs)
+
+    loss = {
+        'pitch': tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True),
+        'step': mse_with_positive_pressure,
+        'duration': mse_with_positive_pressure,
+    }
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    model.compile(loss=loss, optimizer=optimizer)
+
+    model.summary()
+
+    losses = model.evaluate(train_ds, return_dict=True)
+
+    model.compile(
+        loss=loss,
+        loss_weights={
+            'pitch': 1.0,  # 0.05
+            'step': 1.0,
+            'duration': 1.0,
+        },
+        optimizer=optimizer,
+    )
+    model.evaluate(train_ds, return_dict=True)
+
+    callbacks = [
+        tf.keras.callbacks.ModelCheckpoint(
+            filepath='./training_checkpoints/ckpt_{epoch}',
+            save_weights_only=True),
+        tf.keras.callbacks.EarlyStopping(
+            monitor='loss',
+            patience=5,
+            verbose=1,
+            restore_best_weights=True),
+    ]
+    epochs = 25
+
+    history = model.fit(
+        train_ds,
+        epochs=epochs,
+        callbacks=callbacks,
+    )
+
+    plt.plot(history.epoch, history.history['loss'], label='total loss')
+    plt.show()
+
+    # Generate Notes
+    temperature = 2.0
+    num_predictions = 120
+    sample_file = filenames[142]
+    raw_notes = midi_to_notes(sample_file)
+    sample_notes = np.stack([raw_notes[key] for key in key_order], axis=1)
+
+    # The initial sequence of notes; pitch is normalized similar to training
+    # sequences
+    input_notes = (
+            sample_notes[:seq_length] / np.array([vocab_size, 1, 1]))
+
+    for x in range(2):
+        generated_notes = []
+        prev_start = 0
+        for _ in range(num_predictions):
+            pitch, step, duration = predict_next_note(input_notes, model, temperature)
+            start = prev_start + step
+            end = start + duration
+            input_note = (pitch, step, duration)
+            generated_notes.append((*input_note, start, end))
+            input_notes = np.delete(input_notes, 0, axis=0)
+            input_notes = np.append(input_notes, np.expand_dims(input_note, 0), axis=0)
+            prev_start = start
+
+        generated_notes = pd.DataFrame(
+            generated_notes, columns=(*key_order, 'start', 'end'))
+
+        get_note_names = np.vectorize(pretty_midi.note_number_to_name)
+        sample_note_names = get_note_names(raw_notes['pitch'])
+        print(sample_note_names[:10])
+
+        pm = pretty_midi.PrettyMIDI(sample_file)
+        instrument = pm.instruments[0]
+        instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
+
+        example_file = f"example_{x}.midi"
+        example_pm = notes_to_midi(
+            generated_notes, out_file=example_file, instrument_name=instrument_name)
+        save_audio(example_pm, example_file)
+
+        plot_piano_roll(generated_notes)
+        plot_distributions(generated_notes)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    do_stuff()
+    seed = 42
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    data_dir = pathlib.Path('data/maestro-v2.0.0')
+    if not data_dir.exists():
+        tf.keras.utils.get_file(
+            'maestro-v2.0.0-midi.zip',
+            origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
+            extract=True,
+            cache_dir='.', cache_subdir='data',
+        )
+    filenames = glob.glob(str(data_dir / '**/*.mid*'))
+
+    # do_stuff()
+    train()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
